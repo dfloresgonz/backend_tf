eb1a4673e7ed653f0c898c8b05002a59b69de53f
dfloresgonz <dfloresgonz@gmail.com>
2024-09-22 07:51:20 -0500

    apigateway12

--- infrastructure/services/service1/functionA/main.tf
+++ infrastructure/services/service1/functionA/main.tf
@@ -1,13 +1,80 @@
-module "functionA" {
-  source     = "./functionA.aux"
-  aws_region = var.aws_region
-  ENVIROMENT = var.ENVIROMENT
-  USUARIO_BD = var.USUARIO_BD
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.26"
+    }
+  }
+
+  backend "s3" {}
+
+  required_version = ">= 1.2.0"
 }
-module "common" {
-  source      = "../../../modules/common"
-  rest_api_id = module.functionA.api_gateway.my_api.id
-  integration_dependencies = [
-    module.functionA.integration1_id,
-  ]
+
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  service_name  = "service1"
+  function_name = "functionA"
+}
+
+module "functionA" {
+  source       = "../../../modules/lambda"
+  base_name    = "${local.service_name}-${local.function_name}"
+  handler_path = "bundle.main"
+  filename     = "../../../../services/${local.service_name}/${local.function_name}/lambda.zip"
+  aws_region   = var.aws_region
+  variables = {
+    ENVIROMENT = var.ENVIROMENT
+    USUARIO_BD = var.USUARIO_BD
+  }
+}
+
+module "api_gateway" {
+  source = "../../../modules/apigateway"
+  # aws_region = var.aws_region
+  api_name  = "my_api"
+  path_part = "service1"
+  # integrations = local.combined_integrations
+}
+
+resource "aws_api_gateway_resource" "resource1" {
+  rest_api_id = module.api_gateway.my_api.id
+  parent_id   = module.api_gateway.my_api.root_resource_id
+  path_part   = "functionA"
+}
+
+resource "aws_api_gateway_method" "method1" {
+  rest_api_id   = module.api_gateway.my_api.id
+  resource_id   = aws_api_gateway_resource.resource1.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration1" {
+  rest_api_id             = module.api_gateway.my_api.id
+  resource_id             = aws_api_gateway_resource.resource1.id
+  http_method             = aws_api_gateway_method.method1.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = module.functionA.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_base_path_mapping" "mapping1" {
+  domain_name = module.api_gateway.custom_domain.domain_name
+  api_id      = module.api_gateway.my_api.id
+  stage_name  = aws_api_gateway_deployment.api_stage.stage_name
+  base_path   = "v1/service1"
+}
+
+# output "integration1_id" {
+#   value = aws_api_gateway_integration.integration1.id
+# }
+
+resource "aws_api_gateway_deployment" "api_stage" {
+  rest_api_id = module.api_gateway.my_api.id
+  stage_name  = "test"
+  depends_on  = aws_api_gateway_integration.integration1.id
 }

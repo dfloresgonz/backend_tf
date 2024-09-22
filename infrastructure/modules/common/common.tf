9eff24a2c941314f59d0e1e6a002bfe55691e107
dfloresgonz <dfloresgonz@gmail.com>
2024-09-21 23:57:02 -0500

    apigateway8

--- infrastructure/modules/common/common.tf
+++ infrastructure/modules/common/common.tf
@@ -1,6 +1,12 @@
 # common.tf or shared module
+locals {
+  combined_integrations = flatten([
+    var.integration_dependencies
+  ])
+}
+
 resource "aws_api_gateway_deployment" "api_stage" {
   rest_api_id = var.rest_api_id
   stage_name  = "test"
-  depends_on  = var.integration_dependencies
+  depends_on  = local.combined_integrations
 }

7eaea4ebfc078a66fe58d7d6ff0d11a6164fe677
dfloresgonz <dfloresgonz@gmail.com>
2024-09-21 23:38:58 -0500

    apigateway6

--- infrastructure/modules/apigateway/common.tf
+++ infrastructure/modules/apigateway/common.tf
@@ -1,15 +1,9 @@
 # common.tf or shared module
-
-locals {
-  combined_integrations = merge(module.api_gateway.integrations,
-    [{ "integration1" = aws_api_gateway_integration.integration1 },
-      { "integration2" = aws_api_gateway_integration.integration2 }
-    ]
-  )
-}
-
 resource "aws_api_gateway_deployment" "api_stage" {
   rest_api_id = module.api_gateway.my_api.id
   stage_name  = "test"
-  depends_on  = local.combined_integrations
+  depends_on = [
+    aws_api_gateway_integration.integration1,
+    aws_api_gateway_integration.integration2,
+  ]
 }

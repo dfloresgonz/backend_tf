aad31629829df48d4b17e70d21bcd34707668cf6
dfloresgonz <dfloresgonz@gmail.com>
2024-09-22 21:43:10 -0500

    both functions18

--- .github/scripts/check_domain.sh
+++ .github/scripts/check_domain.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 DOMAIN_NAME=$1
 
+# Obtén el nombre del dominio usando AWS CLI
 RESULT=$(aws apigateway get-domain-names --query "items[?domainName=='${DOMAIN_NAME}'] | [0].domainName" --output text)
 
-if [ "$RESULT" == "None" ]; then
-  echo "{\"exists\": false}"
+# Si el resultado es "None", significa que no existe, de lo contrario, existe.
+if [ "$RESULT" == "None" ] || [ -z "$RESULT" ]; then
+  echo "{\"exists\": \"false\"}"
 else
-  echo "{\"exists\": true}"
+  echo "{\"exists\": \"true\"}"
 fi

ea5bc6f74800bc484500bd5705d18acff28a7999
dfloresgonz <dfloresgonz@gmail.com>
2024-09-22 22:08:58 -0500

    both functions19

--- infrastructure/modules/common/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "integration_dependencies" {
-  type        = list(any)
-  description = "Lista de integraciones para el despliegue"
-}
-
-variable "rest_api_id" {
-  type        = string
-  description = "ID del API Gateway"
-}
